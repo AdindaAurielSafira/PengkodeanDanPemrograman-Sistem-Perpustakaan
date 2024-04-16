fb4b2de406e71a699b75105ec08d423f7e460b26
Adinda Auriel Safira <166285525+AdindaAurielSafira@users.noreply.github.com>
2024-04-17 02:23:50 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PengkodeanDanPemrograman-Sistem-Perpustakaan
+# Perpustakaan-Sederhana-Digilib-FEB-UNDIP
+
+Sistem Perpustakaan Sederhana
+"Digilib FEB UNDIP"
+
+Referensi GitHub: https://github.com/AgungP88/php-perpustakaan_sederhana
+
+Perbandingan GitHub di bawah dengan Referensi
+
+Erd Sistem Perpustakaan
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>perpus</title>
+<link rel="stylesheet" href="img/csslogin.css">
+</head>
+<body>
+<center>
+
+<form action="login_proses.php" method="post">
+  <h2>DIGILIB FEB UNDIP</h2>
+  <hr>
+  <div class="imgcontainer">
+    <img src="img/login.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <br>
+    <input type="text" placeholder="NIM" name="name" required><br>
+
+    <br>
+    <input type="password" placeholder="Password" name="pw" required><br>
+    <br>
+    <button type="submit">Login</button><br>
+</center>
+</form>
+
+</body>
+</html>
